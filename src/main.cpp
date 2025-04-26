4cea0aa58ea8649ee88e64cee9ddd86f3ea472d9
Noah Wang <bwang.noah@gmail.com>
2025-04-25 22:28:30 -0700

    added helper files to linkage and added user input

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
+#include "../helpers/io.h"
 #include "text_analyzer.h"
 #include <string>
+#include <string_view>
 #include <iostream>
 
-int main()
-{
-    wordCount("day,");
+void outputTextAnalysis(std::string_view text) {
+    std::cout << "There are " << characterCount(text) << " character(s) in your text." << '\n';  
+    std::cout << "There are " << vowelCount(text) << " vowel(s) in your text." << '\n';  
+    std::cout << "There are " << consonantCount(text) << " consonant(s) in your text." << '\n';  
+    std::cout << "There are " << wordCount(text) << " word(s) in your text." << '\n';  
+}
+
+int main() {
+    std::string text{ readText("Please enter a text: ") };
+    outputTextAnalysis(text); 
+     
     return 0;
 }
